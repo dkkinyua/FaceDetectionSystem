b88ec54c216f14d6598998daf057a3512d6bba22
unknown <denzelkinyua11@gmail.com>
2026-01-30 13:18:16 +0300

    Commit: Get blendshapes, pose and landmarks per frame

--- process_webcam.py
+++ process_webcam.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import mediapipe as mp
+from collections import deque
 from mediapipe.tasks.python import vision
 
 model_path = r"data\face_landmarker.task"
@@ -13,24 +14,34 @@ FaceLandmarkerResult = vision.FaceLandmarkerResult
 VisionRunningMode = vision.RunningMode
 
 latest_result = None
+WINDOW_SIZE = 30
+frame_buffer = deque(maxlen=WINDOW_SIZE)
+
 # create face landmarker instance
-def print_result(result, output_image: mp.Image, timestamp_ms: int):
+def result_callback(result, output_image: mp.Image, timestamp_ms: int):
     global latest_result
     latest_result = result
 
 options = FaceLandmarkerOptions(
-    base_options = BaseOptions(model_asset_path=model_path),
-    running_mode = VisionRunningMode.LIVE_STREAM,
-    num_faces=1,
-    output_face_blendshapes=False,
-    output_facial_transformation_matrixes=False,
-    result_callback = print_result
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    num_faces=1,             
+    min_face_detection_confidence=0.5,
+    min_face_presence_confidence=0.5,
+    min_tracking_confidence=0.5,
+    output_face_blendshapes=True,
+    output_facial_transformation_matrixes=True,
+    result_callback=result_callback
 )
 
 landmarker = FaceLandmarker.create_from_options(options)
 
 def process_webcam(landmarker):
     cap = cv2.VideoCapture(0)
+    #reduce cpu load
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
     start_time = time.time()
 
     while cap.isOpened():
@@ -50,12 +61,22 @@ def process_webcam(landmarker):
 
         # Draw face mesh if result exists
         if latest_result and latest_result.face_landmarks:
-            for face_landmarks in latest_result.face_landmarks:
-                for landmark in face_landmarks:
-                    h, w, _ = frame.shape
-                    x = int(landmark.x * w)
-                    y = int(landmark.y * h)
-                    cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+            frame_data = {
+                "timestamp": timestamp_ms,
+                "landmarks": latest_result.face_landmarks[0],
+                "blendshapes": (
+                    latest_result.face_blendshapes[0]
+                    if latest_result.face_blendshapes else None
+                ),
+                "pose": (
+                    latest_result.facial_transformation_matrixes[0]
+                    if latest_result.facial_transformation_matrixes else None
+                ),
+            }
+
+            frame_buffer.append(frame_data) # append frame data to deque
+            if len(frame_buffer) == WINDOW_SIZE:
+                print("Temporal window obtained.") 
 
         cv2.imshow("Face Mesh (Production)", frame)
 
